ffe3ed8eddc78a410d58241b4c56a4666ed85ab7
Grufoony <gregorio_2000@hotmail.it>
2021-03-05 22:51:26 +0100

    censura

--- bot_with_lib.py
+++ bot_with_lib.py
@@ -2,6 +2,11 @@ from telegram.ext import Updater
 from telegram.ext import CommandHandler, MessageHandler, Filters
 import logging
 
+def sub(string, substring):
+    start = string.find(substring) + 1
+    end = len(substring) + start - 1
+    ast = '*'*len(substring)
+    return string[:start] + ast + string[end:]
 
 token = '1505488376:AAFHVl4kNUSrsTpV_Pve8MFT6RFmEJArAlk'
 
@@ -13,14 +18,18 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 def start(update, context):
     context.bot.send_message(chat_id = update.effective_chat.id, text = 'Test')
 
-def echo(update, context):
+def misc(update, context):
     if 'piedi' in update.message.text:
         context.bot.send_photo(chat_id = update.effective_chat.id, photo = 'https://www.benesserecorpomente.it/wp-content/uploads/2017/03/Piedi.jpg')
+    if 'registrazioni' in update.message.text:
+        text = update.message.text
+        context.bot.send_message(chat_id = update.effective_chat.id, text = sub(text, 'registrazioni'))
+        context.bot.delete_message(chat_id = update.effective_chat.id, message_id = update.message.message_id)
 
 start_handler = CommandHandler('start', start)
-echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+misc_handler = MessageHandler(Filters.text & (~Filters.command), misc)
 
 dispatcher.add_handler(start_handler)
-dispatcher.add_handler(echo_handler)
+dispatcher.add_handler(misc_handler)
 
 updater.start_polling()
