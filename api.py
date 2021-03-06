179799ef8bd6e269c614c6c0bf8856cecbebe050
RiccardoBarbieri <43068188+RiccardoBarbieri@users.noreply.github.com>
2021-03-06 23:44:10 +0100

    added location to final schedule dict

--- api.py
+++ api.py
@@ -8,8 +8,8 @@ class UniboAPI():
     def get_orario(corso, anno, date_start, date_end, curricula = '000-000'):
 
 
-        url = 'https://corsi.unibo.it/magistrale/{corso}/orario-lezioni/@@orario_reale_json?anno={anno}&start={date_start}&end={date_end}&curricula={curricula}'
-        r = requests.get()
+        url = f'https://corsi.unibo.it/magistrale/{corso}/orario-lezioni/@@orario_reale_json?anno={anno}&start={date_start}&end={date_end}&curricula={curricula}'
+        r = requests.get(url)
 
         if r.status_code != 200:
             print('Request error')
@@ -33,7 +33,7 @@ class UniboAPI():
     @staticmethod
     def get_orario(corso, anno, date_exact, curricula = '000-000'):
 
-        url = 'https://corsi.unibo.it/magistrale/{corso}/orario-lezioni/@@orario_reale_json?anno={anno}&start={date_exact}&end={date_exact}&curricula={curricula}'
+        url = f'https://corsi.unibo.it/magistrale/{corso}/orario-lezioni/@@orario_reale_json?anno={anno}&start={date_exact}&end={date_exact}&curricula={curricula}'
         r = requests.get(url)
 
         if r.status_code != 200:
@@ -44,14 +44,21 @@ class UniboAPI():
             schedule = json.loads(r.text)
         except JSONDecodeError:
             print('JSON decoding error')
+        
+        try:
+            location = '{aula} {piano}, {ubicazione}'.format(aula = schedule['aule'][0]['des_risorsa'], piano = schedule['aule'][0]['des_piano'], ubicazione = schedule['aule'][0]['des_ubicazione'])
+        except KeyError:
+            location = 'Non disponibile'
 
         delete = ['note', 'end', 'val_crediti', 'aule', 'cod_sdoppiamento', 'extCode', 'periodo']
         
         for i in delete:
             schedule.pop(i, None)
         
-        i['start'] = i['start'][:-9]
-        i['date'] = i.pop('start')
+        schedule['start'] = i['start'][:-9]
+        schedule['date'] = i.pop('start')
+
+        schedule['location'] = location
         
         return schedule
 
