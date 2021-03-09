5645c6cb15b3a6d84bf2971ad60d8f5dd8c1f218
RiccardoBarbieri <43068188+RiccardoBarbieri@users.noreply.github.com>
2021-03-09 15:04:17 +0100

    base script for database in test

--- test.py
+++ test.py
@@ -1,3 +1,25 @@
-import getpass
+import sqlalchemy as db
+import pandas as pd
 
-print(getpass.getuser())
+
+engine = db.create_engine('mysql+pymysql://root@127.0.0.1:3306/telegrambot')
+
+meta = db.MetaData()
+
+data = db.Table('data', meta, 
+                db.Column('chat_id', db.Integer, primary_key = True),
+                db.Column('user_id', db.Integer, primary_key = True),
+                db.Column('course', db.String(25)),
+                db.Column('detail', db.Integer),
+                db.Column('year', db.Integer))
+
+meta.bind = engine
+meta.create_all(engine)
+
+
+insert = data.insert()
+values = [{'chat_id':124, 'user_id':1232, 'course':'test', 'detail':1, 'year':2}]
+
+connection = engine.connect()
+ResultProxy = connection.execute(insert, values)
+
