095f7b2629bdc67a8075ac4fa6823ee46332c84b
RiccardoBarbieri <43068188+RiccardoBarbieri@users.noreply.github.com>
2021-03-10 12:01:49 +0100

    created table data

--- sqlite_test.py
+++ sqlite_test.py
@@ -9,5 +9,26 @@ def create_connection(db_file):
         print(sqlite3.version)
     except sqlite3.Error as e:
         print(e)
+    return conn
 
-create_connection('telegram.db')
+connection = create_connection('telegram.db')
+
+cursor = connection.cursor()
+
+# creazione tabella
+# cursor.execute('''CREATE TABLE IF NOT EXISTS data (
+#                   chat_id INTEGER,
+#                   user_id INTEGER,
+#                   course TEXT,
+#                   year INTEGER CHECK (year >= 1 AND year <= 3),
+#                   detail INTEGER CHECK (detail >= 1 AND detail <= 3),
+#                   curricula TEXT,
+#                   PRIMARY KEY (chat_id, user_id)
+#                   ) WITHOUT ROWID;''')
+
+# stampa tabelle esistenti
+# cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+# print((cursor.fetchall()))
+
+connection.commit()
+connection.close()
