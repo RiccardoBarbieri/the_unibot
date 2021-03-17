372dac07ddb9183b2f100a8a7851e4edc51b2ec4
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2021-03-17 22:14:26 +0100

    set up for raspi

--- test/test_database_class.py
+++ test/test_database_class.py
@@ -1,9 +1,13 @@
 import sys
 import getpass
 if getpass.getuser() == 'ricca':
-    sys.path.append('C:\\Users\\ricca\\Desktop\\telegram') #TODO: change this path when migrating to another platform
+    sys.path.append('C:\\Users\\ricca\\Desktop\\telegram')
 elif getpass.getuser() == 'grufoony':
     sys.path.append('/home/grufoony/bot-telegram')
+elif getpass.getuser() == 'riccardoob':
+    sys.path.append('/home/riccardoob/telegram_bot')
+elif getpass.getuser() == 'pi':
+    sys.path.append('/home/pi/telegram-bot')
 import sqlite3
 import json
 from database.database import Database
