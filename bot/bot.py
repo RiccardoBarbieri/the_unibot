57c6124b7c153264e6c4cc800494ce7630243a67
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2021-03-18 22:59:25 +0100

    refactor of database - creation courses and curriculas

--- bot/bot.py
+++ bot/bot.py
@@ -94,10 +94,14 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 # TODO: change with sql query when courses ar in tables
 # TODO: gestici curricula non ancora imposta in orario!!!!
+# TODO: aggiungi backup a ogni aggiornamento
+# TODO: rifai a classe
+# TODO: tastiera solo per utente che richiede
+# TODO: controlla che risposta sia dell'utente che manda il comando
 
 
 def start(update, context):
-    db.insert(update.effective_chat.id, update.effective_user.id)
+    db.insert('data', chat_id = update.effective_chat.id, user_id = update.effective_user.id, course = 0, year = 1, detail = 2, curricula = '000-000')
     context.bot.send_message(chat_id=update.effective_chat.id, text='Benvenuto/a dal bot dell\'Università di Bologna.\nPer una guida rapida è possibile consultare la <a href="{link}">repository</a> del bot.'
                              .format(link='https://github.com/RiccardoBarbieri/t_bot'), parse_mode=ParseMode.HTML, disable_web_page_preview=True)
 
@@ -134,9 +138,9 @@ def misc(update, context):
             course_name=course_name, link=found['site'])
         context.bot.send_message(
             chat_id=chat_id, text=message, reply_markup=telegram.ReplyKeyboardRemove(), parse_mode=ParseMode.HTML, disable_web_page_preview=True)
-        db.insert(chat_id, user_id)
-        db.update(chat_id, user_id, course=course_code)
-        db.backup()
+        db.insert('data', chat_id, user_id, course = 0, year = 1, detail = 2, curricula = '000-000')
+        db.update('data', chat_id, user_id, course=course_code)
+        db.backup('data')
         print('Updated user {user_id} with course {course_code}'.format(
             course_code=course_code, user_id=user_id))
 
@@ -149,6 +153,8 @@ def set_corso(update, context):
     with open(Path('./resources/flat_courses.json')) as f:
         courses = json.load(f)
     last_command = update.message
+
+    
 
     def pages_creation(courses, page_num):
         pages = []
@@ -223,7 +229,7 @@ def set_corso(update, context):
 
     if pages:  # if pages is not empty
         keyboard = telegram.ReplyKeyboardMarkup(
-            pages[page_param], one_time_keyboard=True)
+            pages[page_param], one_time_keyboard=True, selective=True)
         context.bot.send_message(chat_id=update.effective_chat.id, text='Seleziona il corso, {page_param}/{pages}.'.format(
             pages=page_num, page_param=page_param + 1), reply_markup=keyboard)
     else:
@@ -245,8 +251,8 @@ def set_anno(update, context):
         if params['numeric'][0] >= 1 or params['numeric'][0] <= 5:
             chat_id = last_command.chat.id
             user_id = last_command.from_user.id
-            db.insert(chat_id, user_id)
-            db.update(chat_id, user_id, year=params['numeric'][0])
+            db.insert('data', chat_id, user_id, course = 0, year = 1, detail = 2, curricula = '000-000')
+            db.update('data', chat_id, user_id, year=params['numeric'][0])
             context.bot.send_message(chat_id=update.effective_chat.id,
                                      text='Impostato anno a {year}.'.format(year=params['numeric'][0]))
             print(db.query_by_ids(chat_id, user_id))
@@ -265,8 +271,8 @@ def set_detail(update, context):
         if params['numeric'][0] >= 1 or params['numeric'][0] <= 5:
             chat_id = last_command.chat.id
             user_id = last_command.from_user.id
-            db.insert(chat_id, user_id)
-            db.update(chat_id, user_id, detail=params['numeric'][0])
+            db.insert('data', chat_id, user_id)
+            db.update('data', chat_id, user_id, detail=params['numeric'][0])
             context.bot.send_message(chat_id=update.effective_chat.id,
                                      text='Impostato dettaglio a {detail}.'.format(detail=params['numeric'][0]))
             print(db.query_by_ids(chat_id, user_id))
