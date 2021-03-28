8e8c3edf660d20956123c6c7e7a26bb19eca08f5
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2021-03-29 00:24:28 +0200

    creating new database

--- temp.py
+++ temp.py
@@ -4,23 +4,17 @@ from pathlib import Path
 from database.database import Database
 from utils.utils import Utils
 from typing import Any
+from mysql import connector
+from mysql.connector.connection import CursorBase
+import requests
 
-class Test():
+def func(*where):
+    print(where)
 
-    def __init__(self, attrs: dict):
-        self.__dict__.update(attrs) # comment to use __getattr__
-        # self.__attrs = attrs # uncomment to use __getattr__
+func(data.user_id == 'asd')
 
-    # def __getattr__(self, name: str) -> Any: # uncomment to use __getattr__
-    #     return self.__attrs[name] 
-    
+# -1001430472485, # gruppo amici greg
+# -1001404621927, # testiamolo
 
-a = Test({'chat_id':1234, 'user_id':4321, 'atosend_time':'23:23'})
-
-print(a.chat_id)
-
--1001430472485, # gruppo amici greg
--1001404621927, # testiamolo
-
-131125356 # io
-913871757 # greg
+# 131125356 # io
+# 913871757 # greg
