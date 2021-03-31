b800ef6e58b970ebc03d0629bb4cd74679fa55cd
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2021-04-01 01:11:50 +0200

    creating metadata system and usage in wrappers

--- new_database/wrappers/update_wrapper.py
+++ new_database/wrappers/update_wrapper.py
@@ -10,9 +10,49 @@ elif getpass.getuser() == 'riccardoob':
 elif getpass.getuser() == 'pi':
     sys.path.append('/home/pi/telegram-bot')
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any, Dict, List, AnyStr
+
+from new_database.exceptions import WrongClauseOrder
 
 if TYPE_CHECKING:
     from new_database.statements.update import Update
+    from new_database.model.table import Table
+    from new_database.model.column import Column
+    from new_database.metadata import MetaData
+
+class UpdateWrapper():
+
+    __metadata: MetaData
+
+    __table: Table
+    __set_clause: Dict[Column, Any]
+    __where_clause: Dict[Column, Any]
+
+    __table_str: AnyStr
+    __set_clause_str: List[AnyStr]
+    __where_clause_str: List[AnyStr]
 
 
+    def __init__(self, metadata, table_str: AnyStr = None, set_clause_str: List[AnyStr] = None, where_clause_str List[AnyStr] = None):
+        self.__metadata = metadata
+
+        self.__table_str = table_str
+        self.__set_clause_str = set_clause_str
+        self.__where_clause_str = where_clause_str
+
+        # parse strings using metadata to those structures
+    
+    def __str__(self) -> str:
+        return str(Update(self.__table, self.__set_clause, self.__where_clause))
+
+    def update(self, table: str) -> UpdateWrapper:
+        return UpdateWrapper(self.__metadata, table, self.__set_clause, self.__where_clause)
+
+    def set(self, set_clause: List[AnyStr]) -> UpdateWrapper:
+        return UpdateWrapper(self.__metadata, self.__table, set_clause, self.__where_clause)
+
+    def where(self, where_clause: List[AnyStr]) -> UpdateWrapper:
+        if not self.__set_clause:
+            raise WrongClauseOrder('You have to specify the set clause before')
+        return UpdateWrapper(self.__metadata, self.__table, self.__set_clause, where_clause)
+    
