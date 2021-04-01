2aeab4e4058cc8f558a49d79e87cd93e3e2e95ee
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2021-04-01 14:29:38 +0200

    creating update and select wrapper

--- new_database/wrappers/select_wrapper.py
+++ new_database/wrappers/select_wrapper.py
@@ -18,6 +18,7 @@ if TYPE_CHECKING:
     from new_database.model.table import Table
     from new_database.model.column import Column
     from new_database.metadata import MetaData
+    from new_database.statements.select import Select
 
 class SelectWrapper():
 
@@ -28,16 +29,23 @@ class SelectWrapper():
     __where_clause: Dict[Table, Dict[Column, Any]]
 
     __select_clause_str: List[AnyStr]
-    __from_tables_str: List[AnyStr]
     __where_clause_str: List[AnyStr]
 
-    def __init__(self, metadata, select_clause_str: List[AnyStr] = None, from_tables_str: List[AnyStr] = None, where_clause_str: List[AnyStr] = None):
+    def __init__(self, metadata, select_clause_str: List[AnyStr] = None, where_clause_str: List[AnyStr] = None):
         self.__metadata = metadata
 
-        self.__from_tables_str = from_tables_str
         self.__select_clause_str = select_clause_str
         self.__where_clause_str = where_clause_str
 
-        # parse bla bla bla
+        # parse bla bla bla, tables are parse from select clause (list of str pf type table.column or only table if SELECT *)
 
-    def select(self, )
+    def __str__(self) -> str:
+        return str(Select(self.__select_clause, self.__from_tables, self.__where_clause))
+
+    def select(self, select_clause_str: List[AnyStr]):
+        return SelectWrapper(select_clause_str, self.__where_clause_str)
+
+    def where(self, where_clause_str):
+        if not self.__select_clause_str:
+            raise WrongClauseOrder('You have to specify the select clause before')
+        return SelectWrapper(self.__select_clause_str, where_clause_str)
