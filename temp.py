7055f3e017094c2ba7511accd41ec12afa4f09d3
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2021-04-06 00:09:34 +0200

    bugfix in selectwrapper

--- temp.py
+++ temp.py
@@ -1,21 +1,11 @@
-import pprint
-import json
-from pathlib import Path
-from database.database import Database
-from utils.utils import Utils
-from typing import Any
-from mysql import connector
-from mysql.connector.connection import CursorBase
-import requests
-
-
-
-list1 = [1234, 4321]
-
-list2 = ['123']
-
-list1 += list2
-
-print(list1)
-
-
+from __future__ import annotations
+import sys
+import getpass
+if getpass.getuser() == 'ricca':
+    sys.path.append('C:\\Users\\ricca\\Desktop\\telegram')
+elif getpass.getuser() == 'grufoony':
+    sys.path.append('/home/grufoony/bot-telegram')
+elif getpass.getuser() == 'riccardoob':
+    sys.path.append('/home/riccardoob/telegram_bot')
+elif getpass.getuser() == 'pi':
+    sys.path.append('/home/pi/telegram-bot')
