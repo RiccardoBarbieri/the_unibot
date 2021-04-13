04a48cbfc2346798220c87d7d50bd04960821772
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2021-04-14 00:45:48 +0200

    docs and unused imports removal

--- simple_sql/wrappers/delete_wrapper.py
+++ simple_sql/wrappers/delete_wrapper.py
@@ -21,6 +21,36 @@ if TYPE_CHECKING:
 from simple_sql.statements.delete import Delete
 
 class DeleteWrapper():
+    """
+    This class uses the table name and the where clause as strings
+    with the metadata object (handled by ``Database``) to obtain
+    the correct ``Table`` object and to create the where clause.
+    Using the delete and where methods, ``DeleteWrapper`` instances will be
+    created and returned to add clauses separately.
+    In the future all where clauses will be modeled with a class.
+
+    Parameters
+    ----------
+    metadata: MetaData, optional
+        The ``MetaData`` instance of the database.
+    table_str: AnyStr, optional
+        The name of the table to delete from.
+    where_clause_str: Dict[AnyStr, Any], optional
+        The where clause to restrict which entries to delete.
+
+    Attributes
+    ----------
+    __metadata: MetaData
+        The ``MetaData`` instance of the database.
+    __table: Table
+        The ``Table`` object.
+    __where_clause: Dict[Column, Any]
+        The where clause generated from the string where clause.
+    __table_str: AnyStr
+        The name of the table to delete from.
+    __where_clause_str: Dict[AnyStr, Any]
+        The where clause to restrict which entries to delete.
+    """
 
     __metadata: MetaData
 
@@ -48,10 +78,32 @@ class DeleteWrapper():
         
 
     def __str__(self) -> str:
+        """
+        Calls the ``Delete`` object string representation.
+
+        Returns
+        -------
+        str
+            MySQL compliant string of the delete statement execution.
+        """
         return str(Delete(self.__table, self.__where_clause))
 
     # def delete_from(self, table_str: AnyStr) -> DeleteWrapper:
     #     return DeleteWrapper(self.__metadata, table_str, self.__where_clause_str)
 
     def where(self, where_clause_str: Dict[AnyStr, Any]) -> DeleteWrapper:
+        """
+        Used to specify the where clause.
+
+        Parameters
+        ----------
+        where_clause_str: Dict[AnyStr, Any]
+            The where clause to restrict which entries to delete.
+        
+        Returns
+        -------
+        DeleteWrapper
+            A ``DeleteWrapper`` instance with the same metadata and table
+            name and the where_clause_str passed as parameter.
+        """
         return DeleteWrapper(self.__metadata, self.__table_str, where_clause_str)
--- simple_sql/wrappers/update_wrapper.py
+++ simple_sql/wrappers/update_wrapper.py
@@ -22,6 +22,45 @@ from simple_sql.statements.update import Update
 
 class UpdateWrapper():
 
+    """
+    This class uses the table name, the set clause and where clause
+    as strings with the metadata object (handled by ``Database``) to obtain
+    the correct ``Table`` object and to create the where and set clauses.
+    Using the update, set and where methods, ``UpdateWrapper`` instances will be
+    created and returned to add clauses separately.
+    In the future all where clauses will be modeled with a class.
+
+    Parameters
+    ----------
+    metadata: MetaData, optional
+        The ``MetaData`` instance of the database.
+    table_str: AnyStr, optional
+        The name of the table to update.
+    set_clause_str: Dict[AnyStr, Str], optional
+        The set clause containing which column to update
+        and with which value.
+    where_clause_str: Dict[AnyStr, Str], optional
+        The where clause to restrict which entries to updates.
+
+    Attributes
+    ----------
+    __metadata: MetaData, optional
+        The ``MetaData`` instance of the database.
+    __table: Table
+        The ``Table`` object to update.
+    __set_clause: Dict[AnyStr, Any]
+        The set clause generated from the string set clause.
+    __where_clause: Dict[AnyStr, Any]
+        The where clause generated from the string where clause.
+    __table_str: AnyStr, optional
+        The name of the table to update.
+    __set_clause_str: Dict[AnyStr, Str], optional
+        The set clause containing which column to update
+        and with which value.
+    __where_clause_str: Dict[AnyStr, Str], optional
+        The where clause to restrict which entries to updates.
+    """
+
     __metadata: MetaData
 
     __table: Table
@@ -57,15 +96,52 @@ class UpdateWrapper():
             self.__set_clause[col_obj] = self.__set_clause_str[col]
     
     def __str__(self) -> str:
+        """
+        Calls the ``Update`` object string representation.
+
+        Returns
+        -------
+        str
+            MySQL compliant string of the update statement execution.
+        """
         return str(Update(self.__table, self.__set_clause, self.__where_clause))
 
     # def update(self, table_str: AnyStr) -> UpdateWrapper:
     #     return UpdateWrapper(self.__metadata, table_str, self.__set_clause_str, self.__where_clause_str)
 
     def set(self, set_clause_str: Dict[AnyStr, Any]) -> UpdateWrapper:
+        """
+        Used to specify the set clause.
+
+        Parameters
+        ----------
+        set_clause_str: Dict[AnyStr, Any]
+            The set clause containing which column to update
+            and with which value.
+        
+        Returns
+        -------
+        UpdateWrapper
+            A ``UpdateWrapper`` instance with the same metadata, table
+            name and where clause and the set_clause_str passed as parameter.
+        """
         return UpdateWrapper(self.__metadata, self.__table_str, set_clause_str, self.__where_clause_str)
 
     def where(self, where_clause_str: Dict[AnyStr, Any]) -> UpdateWrapper:
+        """
+        Used to specify the where clause.
+
+        Parameters
+        ----------
+        where_clause_str: Dict[AnyStr, Any]
+            The where clause to restrict which entries to update.
+        
+        Returns
+        -------
+        UpdateWrapper
+            A ``UpdateWrapper`` instance with the same metadata, table
+            name and set clause and the where_clause_str passed as parameter.
+        """
         if not self.__set_clause:
             raise WrongClauseOrder('You have to specify the set clause before')
         return UpdateWrapper(self.__metadata, self.__table_str, self.__set_clause_str, where_clause_str)
