8538ab9d95d6613d32c824d7ac6f5639bdd760eb
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2021-04-22 00:12:52 +0200

    course complete - docs update

--- temp.py
+++ temp.py
@@ -10,27 +10,36 @@ elif getpass.getuser() == 'riccardoob':
 elif getpass.getuser() == 'pi':
     sys.path.append('/home/pi/telegram-bot')
 
-class class1():
+# class class1():
     
-    def __init__(self, valore: int):
-        self.val = valore
+#     def __init__(self, valore: int):
+#         self.val = valore
     
-    def __ge__(self, o: object):
+#     def __ge__(self, o: object):
         
     
-class class2():
+# class class2():
     
-    def __init__(self) -> None:
-        self.asd = '123'
+#     def __init__(self) -> None:
+#         self.asd = '123'
     
-    def __ge__(self, o: object):
-        if o.val == int(o.val):
-            return class1(124)
+#     def __ge__(self, o: object):
+#         if o.val == int(o.val):
+#             return class1(124)
         
-def func(class1: class1):
-    print(class1.val)
+# def func(class1: class1):
+#     print(class1.val)
 
-c = class1(123)
-cc = class2()
+# c = class1(123)
+# cc = class2()
 
-func(cc >= c)
+# func(cc >= c)
+
+a = {
+    'ads': 123
+}
+
+try:
+    print(a['asd'])
+except KeyError as e:
+    print(e)
