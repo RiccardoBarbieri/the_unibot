7874f9fbbd78ce9a61a58dc19bfc52bb25a6024e
Riccardo Barbieri <riccardo_barbieri@outlook.it>
2021-11-09 18:06:25 +0100

    teachings log

--- test/test_database_class.py
+++ test/test_database_class.py
@@ -1,16 +1,6 @@
-import sys
-import getpass
-if getpass.getuser() == 'ricca':
-    sys.path.append('C:\\Users\\ricca\\Desktop\\telegram')
-elif getpass.getuser() == 'grufoony':
-    sys.path.append('/home/grufoony/bot-telegram')
-elif getpass.getuser() == 'riccardoob':
-    sys.path.append('/home/riccardoob/telegram_bot')
-elif getpass.getuser() == 'pi':
-    sys.path.append('/home/pi/telegram-bot')
 import sqlite3
 import json
-from database.database import Database
+from the_unibot.database import Database
 from pathlib import Path
 
 db = Database(Path('./database/telegram.db'))
