411df1e20e23f4ddb8d41c435520e02b794d1174
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2021-11-13 16:33:20 +0100

    new parse courses scrpit - downloaded teachings

--- parsers/scrape_teachings.py
+++ parsers/scrape_teachings.py
@@ -3,6 +3,7 @@ import json
 import datetime
 from the_unibot.database import Database
 from the_unibot.utils import Utils
+from tqdm import tqdm
 
 class bcolors:
     HEADER = '\033[95m'
@@ -16,15 +17,16 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 if __name__ == '__main__':
-    with open("./resources/flat_courses_full.json") as f:
+    with open("./resources/flat_courses_curriculas.json") as f:
         courses = json.load(f)
     db = Database("./database/telegram.db")
 
-    years = ["2018", "2019", "2020", "2021"]
+    years = ["2016", "2017", "2018", "2019", "2020", "2021"]
 
-    with open("parsers/teachings.log.md", "w+") as f:
+    f = open("parsers/teachings.log.md", "w+")
 
-        for i in courses:
+    try:
+        for i, j in zip(courses, tqdm(range(len(courses)))):
             lang = Utils.get_course_lang2(i["site"])
             year = now = datetime.datetime.now().year
             course_code = i["course_code"]
@@ -34,11 +36,18 @@ if __name__ == '__main__':
                     url = i["site"] + f"/{lang}/piano/{year}/{course_code}/{curricula}/{y}"
                     r = requests.get(url)
                     if r.status_code != 200:
-                        print(bcolors.FAIL + 'error   ' + bcolors.ENDC + f' on {i["course_name"]}, {url}')
+                        tqdm.write(bcolors.FAIL + 'error   ' + bcolors.ENDC + f' on {i["course_name"]}, {url}')
                         f.write('<span style="color:red;font-family:courier-new;font-size:20px">error   </span>' + f' on {i["course_name"]}, {url}\n')
                     else:
-                        print(bcolors.OKGREEN + 'success ' + bcolors.ENDC + f' on {i["course_name"]}, {url}')
+                        tqdm.write(bcolors.OKGREEN + 'success ' + bcolors.ENDC + f' on {i["course_name"]}, {url}')
                         f.write('<span style="color:green;font-family:courier-new;font-size:20px">success </span>' + f' on {i["course_name"]}, {url}\n')
+                        with open(f'./resources/teachings/{course_code}.{j["value"]}.{year}.html', "w+") as f2:
+                            f2.write(r.content.decode("utf-8"))
+    except KeyboardInterrupt as e:
+        print(e)
+    finally:
+        f.close()
+            
 
 
 
