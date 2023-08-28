cdf4f4d2e8bed81994758008ef52e7e3ce84aa1d
Grufoony <gregorio_2000@hotmail.it>
2023-08-28 14:49:51 +0200

    docstrings

--- api/weather.py
+++ api/weather.py
@@ -3,10 +3,39 @@ from datetime import datetime
 from datetime import timedelta
 from pathlib import Path
 
+'''
+This class is used to get the weather forecasts using the OpenWeatherMap API.
+
+Attributes
+----------
+None
+
+Methods
+-------
+get_weather(city: str, day: int) -> str
+    Returns the weather forecast for the specified city and day.
+parse_weather(day: int, weather: dict) -> str
+    Returns the weather forecast for the specified day.
+'''
 class WeatherAPI():
     
+    '''
+    Returns the weather forecast for the specified city and day.
+    
+    Parameters
+    ----------
+    city : str
+        The city for which the weather forecast is requested.
+    day : int
+        The day for which the weather forecast is requested.
+        
+    Returns
+    -------
+    str
+        The weather forecast for the specified city and day.
+    '''
     @staticmethod
-    def get_weather(city: str, day: int):
+    def get_weather(city: str, day: int) -> str:
         with open(Path('./keys/weather.txt')) as f:
             api_key = f.readline()
         coords_url = "https://api.openweathermap.org/data/2.5/weather?&appid={key}&q={city}&units=metric".format(key = api_key, city = city)
@@ -22,8 +51,23 @@ class WeatherAPI():
         else:
             return 'Meteo non disponibile'
 
+    '''
+    Returns the parsed forecast given weather and day.
+
+    Parameters
+    ----------
+    day : int
+        The day for which the weather forecast is requested.
+    weather : dict
+        The weather forecast for the specified city and day.
+        
+    Returns
+    -------
+    str
+        The weather forecast for the specified city and day.
+    '''
     @staticmethod
-    def parse_weather(day: int, weather: dict):
+    def parse_weather(day: int, weather: dict) -> str:
         temp_min = int(weather['daily'][0]['temp']['min'])
         temp_max = int(weather['daily'][0]['temp']['max'])
         description = weather['daily'][0]['weather'][0]['description']
