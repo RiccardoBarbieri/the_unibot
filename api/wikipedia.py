8de6b46498699c5c360572bd680ec2d374519f40
Grufoony <gregorio_2000@hotmail.it>
2023-08-28 14:49:51 +0200

    docstrings

--- api/wikipedia.py
+++ api/wikipedia.py
@@ -1,11 +1,37 @@
 import wikipediaapi
 import requests
-from json import JSONDecodeError
 
+'''
+This class is used to get the wikipedia pages using the Wikipedia API.
+
+Attributes
+----------
+None
+
+Methods
+-------
+pages(query: str) -> dict
+    Returns the wikipedia pages for the specified query.
+summary(url: str) -> str
+    Returns the summary of the specified wikipedia page.
+'''
 class WikipediaAPI():
-    
+
+    '''
+    Returns the wikipedia pages for the specified query.
+
+    Parameters
+    ----------
+    query : str
+        The query for which the wikipedia pages are requested.
+
+    Returns
+    -------
+    dict
+        The wikipedia pages for the specified query.
+    '''
     @staticmethod
-    def pages(query):
+    def pages(query) -> dict:
         url = 'https://en.wikipedia.org/w/api.php'
         params = {
             'action':'opensearch',
@@ -26,8 +52,21 @@ class WikipediaAPI():
         else:
             return dict(names=data[1][0], links=data[3][0], single=True) # if True -> single link
     
+    '''
+    Returns the summary of the specified wikipedia page.
+    
+    Parameters
+    ----------
+    url : str
+        The url of the wikipedia page for which the summary is requested.
+
+    Returns
+    -------
+    str
+        The summary of the specified wikipedia page.
+    '''
     @staticmethod
-    def summary(url):
+    def summary(url) -> str:
         user_agent = 'the_unibot (https://github.com/RiccardoBarbieri/the_unibot)'
         language = 'en'
         wiki = wikipediaapi.Wikipedia(user_agent=user_agent, language=language)
@@ -36,7 +75,7 @@ class WikipediaAPI():
         index = reverse_url.find('/')
         page_name = reverse_url[:index][::-1]
         
-        # fix bug apostrophes
+        # apostrophes management
         if '%27' in page_name:
             page_name = page_name.replace('%27', '\'')
 
