d36367a7453f2dd866b4219247826e29650840ad
Gregorio Berselli <gregorio.berselli@studio.unibo.it>
2023-10-06 13:34:49 +0200

    implemented tests on main libs (#37)
    * utils tests and docstring
    
    * added wikipediaAPI test
    
    * fix
    
    * weather tests
    
    * unibo tests
    
    * moved files
    
    * `/set_detail` online only tag only if True (#36)
    
    * renewed `MessageCreator`
    
    * tested messgae_creator
    
    * formatting

--- test/sqlite_test.py
+++ test/sqlite_test.py
@@ -1,34 +1,34 @@
-import sqlite3
+# import sqlite3
 
 
-def create_connection(db_file):
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
-    except sqlite3.Error as e:
-        print(e)
-    return conn
+# def create_connection(db_file):
+#     conn = None
+#     try:
+#         conn = sqlite3.connect(db_file)
+#         print(sqlite3.version)
+#     except sqlite3.Error as e:
+#         print(e)
+#     return conn
 
 
-connection = create_connection('telegram.db')
+# connection = create_connection('telegram.db')
 
-cursor = connection.cursor()
+# cursor = connection.cursor()
 
-# creazione tabella
-# cursor.execute('''CREATE TABLE IF NOT EXISTS data (
-#                   chat_id INTEGER,
-#                   user_id INTEGER,
-#                   course TEXT,
-#                   year INTEGER CHECK (year >= 1 AND year <= 3),
-#                   detail INTEGER CHECK (detail >= 1 AND detail <= 3),
-#                   curricula TEXT,
-#                   PRIMARY KEY (chat_id, user_id)
-#                   ) WITHOUT ROWID;''')
+# # creazione tabella
+# # cursor.execute('''CREATE TABLE IF NOT EXISTS data (
+# #                   chat_id INTEGER,
+# #                   user_id INTEGER,
+# #                   course TEXT,
+# #                   year INTEGER CHECK (year >= 1 AND year <= 3),
+# #                   detail INTEGER CHECK (detail >= 1 AND detail <= 3),
+# #                   curricula TEXT,
+# #                   PRIMARY KEY (chat_id, user_id)
+# #                   ) WITHOUT ROWID;''')
 
-# stampa tabelle esistenti
-# cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-# print((cursor.fetchall()))
+# # stampa tabelle esistenti
+# # cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+# # print((cursor.fetchall()))
 
-connection.commit()
-connection.close()
+# connection.commit()
+# connection.close()
--- test/test_database_class.py
+++ test/test_database_class.py
@@ -1,8 +1,8 @@
-import sys  # nopep8
-sys.path.append('.')  # nopep8
-from database import Database
-from pathlib import Path
+# import sys  # nopep8
+# sys.path.append('.')  # nopep8
+# from database import Database
+# from pathlib import Path
 
-db = Database(Path('./database/telegram.db'))
+# db = Database(Path('./database/telegram.db'))
 
-print(db.query_all())
+# print(db.query_all())
