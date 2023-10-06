8d29c1442dbf972f410a2fd78d872a482f29e64a
Gregorio Berselli <gregorio.berselli@studio.unibo.it>
2023-10-06 13:34:49 +0200

    implemented tests on main libs (#37)
    * utils tests and docstring
    
    * added wikipediaAPI test
    
    * fix
    
    * weather tests
    
    * unibo tests
    
    * moved files
    
    * `/set_detail` online only tag only if True (#36)
    
    * renewed `MessageCreator`
    
    * tested messgae_creator
    
    * formatting

--- utils/message_creator.py
+++ utils/message_creator.py
@@ -1,26 +1,38 @@
 import sys  # nopep8
 sys.path.append('.')  # nopep8
-from api import UniboAPI
 import json
 from pathlib import Path
 
+'''
+This class is used to create the message to send to the user
+
+Methods
+-------
+    create_message(schedule, lvl, lang: str) -> str
+        creates the message to send to the user
+'''
+
 
 class MessageCreator():
+    '''
+    This function creates the message to send to the user
 
-    # @staticmethod
-    # def get_lv1_messages(corso, anno, date_start, date_end, curricula = '000-000'):
-    #     schedule = UniboAPI.get_orario(corso, anno, date_start, date_end, curricula)
+    Parameters
+    ----------
+        schedule: dict
+            the schedule of the course
+        lvl: int
+            the level of the message
+        lang: str
+            the language of the message
 
-    # @staticmethod
-    # def get_lv2_messages(corso, anno, date_start, date_end, curricula = '000-000'):
-    #     schedule = UniboAPI.get_orario(corso, anno, date_start, date_end, curricula)
-
-    # @staticmethod
-    # def get_lv3_messages(corso, anno, date_start, date_end, curricula = '000-000'):
-    #     schedule = UniboAPI.get_orario(corso, anno, date_start, date_end, curricula)
-
+    Returns
+    -------
+        string
+            the message to send to the user
+    '''
     @staticmethod
-    def get_message(schedule, lvl, lang: str):
+    def create_message(schedule, lvl, lang: str):
         message = '{time}\n{course_name}'.format(
             course_name=schedule['title'], time=schedule['time'])
 
@@ -39,37 +51,3 @@ class MessageCreator():
             message += '\n<a href="{teams}">{link}</a>'.format(
                 teams=schedule['teams'], link=msg['lecture_link'][lang])
         return message
-
-    @staticmethod
-    def get_lv1_message(corso, anno, date_exact, curricula='000-000'):
-        schedule = UniboAPI.get_orario(corso, anno, date_exact, curricula)
-        message = '{time}\n{course_name}'.format(
-            course_name=schedule['title'], time=schedule['time'])
-        return message
-
-    @staticmethod
-    def get_lv2_message(corso, anno, date_exact, curricula='000-000'):
-        schedule = UniboAPI.get_orario(corso, anno, date_exact, curricula)
-        message = '{time}\n{course_name}\nLuogo: {location}\n<a href="{teams}">link</a>'.format(
-            time=schedule['time'], course_name=schedule['title'], location=schedule['location'], teams=schedule['teams'])
-        return message
-
-    @staticmethod
-    def get_lv3_message(corso, anno, date_exact, curricula='000-000'):
-        schedule = UniboAPI.get_orario(corso, anno, date_exact, curricula)
-        message = '{time}\n{course_name}\nCFU: {cfu}\nDocente: {teacher}\nLuogo: {location}\nTeledidattica obbligatoria: {dad}\nLink lezione: <a href="{teams}">link</a>'\
-            .schedule(time=schedule['time'], course_name=schedule['title'], location=schedule['location'], cfu=schedule['cfu'], teacher=schedule['docente'], dad=schedule['teledidattica'], teams=schedule['teams'])
-        return message
-
-
-# send_message(chat_id=chat_id, text='<b>Example message</b>', parse_mode=telegram.ParseMode.HTML)
-
-# /*
-# 9:00 - 12:00
-# FONDAMENTI DI TELECOMUNICAZIONI T
-# CFU: 9
-# Docente: Carlo Caini
-# Luogo: AULA 6.2 Piano Secondo Facoltà di Ingegneria dell'Università di Bologna
-# Teledidattica obbligatoria: False
-# Link lezione: Teams
-# */
