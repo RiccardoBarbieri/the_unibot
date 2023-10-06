2628e3afd7eb409861523eb07873e6e7c7edde2e
Grufoony <64806874+Grufoony@users.noreply.github.com>
2023-10-06 13:24:18 +0200

    formatting

--- test/test_unibo.py
+++ test/test_unibo.py
@@ -8,11 +8,14 @@ GIVEN a course name, a course type, a course year, a course language, a date and
 WHEN the function is called
 THEN it should return a list of lessons
 '''
+
+
 def test_get_orario_result():
     result = UniboAPI.get_orario(
         'matematica', 'magistrale', '1', 'orario-lezioni', '2023-10-06', '399-000')
     assert isinstance(result, list)
     assert len(result) > 0
+
 
 '''
 This tests if the function UniboAPI.get_orario() works correctly
@@ -20,6 +23,8 @@ GIVEN a course name, a course type, a course year, a course language, a date and
 WHEN the function is called
 THEN it should return an empty list
 '''
+
+
 def test_get_orario_no_results():
     result = UniboAPI.get_orario(
         'Ingegneria del Software', 'laurea-magistrale', '2021', 'it', '2021-10-01', '000-002')
