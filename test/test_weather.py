c370123071bdadf46eb98870d4d6ffab878df2af
Grufoony <64806874+Grufoony@users.noreply.github.com>
2023-10-11 20:41:05 +0200

    added marker to a test

--- test/test_weather.py
+++ test/test_weather.py
@@ -1,6 +1,8 @@
+import sys  # nopep8
+sys.path.append('.')  # nopep8
 from api.weather import WeatherAPI
-import sys
-sys.path.append('.')
+import os
+import pytest
 
 '''
 This tests if the function WeatherAPI.get_weather() works correctly
@@ -10,6 +12,7 @@ THEN it should return a string containing the weather information
 '''
 
 
+@pytest.mark.skipif(not os.path.isfile('keys/weather.txt'), reason='No API key found')
 def test_get_weather():
     result = WeatherAPI.get_weather('Rome', 0, 'en')
     assert isinstance(result, str)
